97ca4fc83c6907a7968a861dcf800827fe84ae3a
Anthony <anthony.capuyan@wlcormoc.edu.ph>
2025-11-11 15:34:44 +0800

    test

--- lib/main.dart
+++ lib/main.dart
@@ -352,7 +352,7 @@ class WelcomePage extends StatelessWidget {
                     // Simple subtitle without neumorphism
                     Center(
                       child: Text(
-                        'Office of Building',
+                        'Office of Building Official',
                         style: TextStyle(
                           fontSize: titleFontSize,
                           color: const Color(0xFF1F2937),
